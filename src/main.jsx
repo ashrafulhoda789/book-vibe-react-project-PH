32d699d88e17be32f56fc80213ac4dadf3927867
ashraful-hoda <asrafulhoda789@gmail.com>
2026-04-11 00:45:02 +0600

    react router setup and component created

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router'
+import { RouterProvider } from 'react-router'
+import { router } from './routes/Routes'
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: "Homepage"
-  },
-  {
-    path: '/books',
-    element: "Books"
-  }
-])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
